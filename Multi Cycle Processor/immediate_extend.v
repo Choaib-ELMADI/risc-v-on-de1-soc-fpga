366683e8818c60ce33e180884d8c4a77147aefaf
ZAKARIA AGHADJOUR <166619958+ZIKOAR@users.noreply.github.com>
2025-04-12 23:30:25 +0100

    Update immediate_extend.v
    HADIIK

--- Multi Cycle Processor/immediate_extend.v
+++ Multi Cycle Processor/immediate_extend.v
@@ -1,19 +1,21 @@
 module immediate_extend (ImmExt, Instr, ImmSrc);
     output reg [31:0] ImmExt;
     input      [31:7] Instr;
-    input       [1:0] ImmSrc;
+    input       [2:0] ImmSrc;
 
     always @(*)
         begin
             case (ImmSrc)
                 // I-type
-                2'b00   : ImmExt <= {{20 {Instr[31]}}, Instr[31:20]};
+                2'b000   : ImmExt <= {{20 {Instr[31]}}, Instr[31:20]};
                 // S-type
-                2'b01   : ImmExt <= {{20 {Instr[31]}}, Instr[31:25], Instr[11:7]};
+                2'b001   : ImmExt <= {{20 {Instr[31]}}, Instr[31:25], Instr[11:7]};
                 // B-type
-                2'b10   : ImmExt <= {{20 {Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0};
+                2'b010   : ImmExt <= {{20 {Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0};
                 // J-type
-                2'b11   : ImmExt <= {{12 {Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1'b0};
+                2'b011   : ImmExt <= {{12 {Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1'b0};
+                // J-type
+                2'b100   : ImmExt <= {instr[31:12], 12'b0};
                 // others
                 default : ImmExt <= 32'bx;
             endcase
